e78966a85aae8c0d03820ec1f7c91dd63af49b36
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2022-12-24 08:35:09 +0100

    Add image  scroll

---
+++ pages/property/[id].js
@@ -0,0 +1,22 @@
+import { Box, Flex, Spacer, Text } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/avatar";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
+
+import { baseUrl, fetchApi } from "../../utils/fetchApi";
+import ImageScrollbar from "../../components/ImageScrollbar";
+const PopertyDetails = () => <Box></Box>;
+
+export default PropertyDetails;
+
+export async function getServerSideProps({ params: { id } }) {
+  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+  return {
+    props: {
+      propertyDetails: data,
+    },
+  };
+}
